dc76551a4936e8a95af980588f30eb0ce61315a8
rdominguez <rdominguez@soaint.com>
2023-01-17 10:12:27 -0500

    Testing Angular material component OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//-------------------------------------------
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    //---------------------------
+    MatCheckboxModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
